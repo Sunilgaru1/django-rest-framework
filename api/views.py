e4cecdb6468387f5e881838f24ec47f69fa5ed3d
sunilgaru1 <sunilgaru097@gmail.com>
2026-06-06 21:11:53 +0530

    Added DRF Serializer POST Method

--- api/views.py
+++ api/views.py
@@ -9,10 +9,17 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentView(request):
     if request.method == 'GET':
         students = Student.objects.all()
         serializer = StudentSerializer(students,many = True)
         return Response(serializer.data,status=status.HTTP_200_OK)
-    
+    elif request.method == 'POST':
+        serializer  = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
