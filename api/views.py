284afc7b514c165e35639238b3c3d1a615b1cba8
sunilgaru1 <sunilgaru097@gmail.com>
2026-06-06 21:11:09 +0530

    Added DRF Serializer GET Method

--- api/views.py
+++ api/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
+from students.models import Student
+from .serializers import StudentSerializer
+
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
 
+@api_view(['GET'])
 def studentView(request):
-    students = [
-        {'id' : 1 , 'name': 'Sunil','age':16}
-    ]
-    return JsonResponse(students,safe=False)
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students,many = True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
